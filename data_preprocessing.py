c8f3f3e87ad5131db36bfaf535001bc6b6f0b5d8
Arpit Agarwal <58038078+aarpit1010@users.noreply.github.com>
2020-12-12 23:55:34 +0530

    Updated files and model

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,13 +8,12 @@ import scipy
 import sklearn
 import scipy.io as sio
 
-def data_clean():
-    df = pd.read_csv('data/dataset.csv')
+def data_clean(PATH, NEW_PATH):
+    df = pd.read_csv(PATH)
     df_ecg = df.drop('3600',axis=1)
     avg = df_ecg.mean(axis=1)
     diff = df_ecg.max(axis=1) - df_ecg.min(axis=1)
     df_ecg=df_ecg.subtract(avg,axis=0)
     df_ecg=df_ecg.div(diff,axis=0)
     df_ecg['3600'] = df['3600']
-    return df_ecg.to_csv('data/preprocessed_dataset.csv', index=False)
-
+    return df_ecg.to_csv(NEW_PATH, index=False)
